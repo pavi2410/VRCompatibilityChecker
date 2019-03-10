b1766f26e78f017fe42961ac5421fe67c3fb9a2c
Pavitra Golchha <pavitragolchha@gmail.com>
2019-03-10 13:38:48 +0530

    Release v7.1 (14)

--- app/src/main/java/appinventor/ai_pavitragolchha/VR/MainActivity.kt
+++ app/src/main/java/appinventor/ai_pavitragolchha/VR/MainActivity.kt
@@ -6,31 +6,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
+
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(my_toolbar)
+        setSupportActionBar(toolbar)
 
         val phoneInfo = PhoneInfo(this)
         val accelerometer = phoneInfo.checkSensor(TYPE_ACCELEROMETER)
         val compass = phoneInfo.checkSensor(TYPE_GRAVITY)
         val gyro = phoneInfo.checkSensor(TYPE_GYROSCOPE)
-        val screenSize = phoneInfo.getScreenSize().toDouble().round()
+        val screenSize = phoneInfo.getScreenSize().round()
         val ram = phoneInfo.getRam()
 
         /*
-            Accelerometer = required
-            Gyro = required
-            Screen Size >= 5"
-            Android >= KitKat
-            RAM >= 2 GB
-        */
+         * Accelerometer = required
+         * Gyro = required
+         * Screen Size >= 5"
+         * Android >= KitKat
+         * RAM >= 2 GB
+         */
         if (accelerometer and gyro and (screenSize >= 5) and (ram >= 2.GB)) {
             message.text = getString(R.string.success_message)
             icon.setImageResource(R.drawable.check)
@@ -44,7 +53,9 @@ class MainActivity : AppCompatActivity() {
                 Item("Compass", compass),
                 Item("Gyroscope", gyro),
                 Item("Screen Size", screenSize >= 5.0),
-                Item("RAM", ram >= 2.GB))
+                Item("RAM", ram >= 2.GB),
+                Item("Android Version", true)
+        )
 
         recyclerView = details_list.apply {
             setHasFixedSize(true)
@@ -53,12 +64,21 @@ class MainActivity : AppCompatActivity() {
 
             adapter = DetailsListAdapter(results)
         }
+
+        // -- AdMob Integration --
+
+        MobileAds.initialize(this, BuildConfig.ADMOB_APP_ID)
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
 
 data class Item(val name: String, val value: Boolean)
 
-// Extension utils
+// -- Extension utils --
+
 private fun Double.round() = Math.round(this * 10) / 10
 
 private val Int.GB: Int
--- app/src/main/java/appinventor/ai_pavitragolchha/VR/PhoneInfo.kt
+++ app/src/main/java/appinventor/ai_pavitragolchha/VR/PhoneInfo.kt
@@ -5,44 +5,30 @@ import android.content.Context
 import android.hardware.SensorManager
 import android.util.DisplayMetrics
 import android.view.WindowManager
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 /**
  * Created by Pavitra on 08-03-2018.
  */
 class PhoneInfo(private val ctx: Context) {
 
-    fun checkSensor(sensor: Int): Boolean {
-        val sm = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val sensorManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val activityManager = ctx.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    private val windowManager = ctx.getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
-        return sm.getSensorList(sensor).size > 0
+    fun checkSensor(sensor: Int) = sensorManager.getSensorList(sensor).isNotEmpty()
+
+    fun getScreenSize() = with(getDisplayMetrics()) {
+        val wi = widthPixels / xdpi // width in inches
+        val hi = heightPixels / ydpi // height in inches
+        Math.hypot(wi.toDouble(), hi.toDouble())
     }
 
-    fun getScreenSize(): Float {
-        val dm = getDisplayMetrics()
-
-        val wi = dm.widthPixels / dm.xdpi // width in inches
-        val hi = dm.heightPixels / dm.ydpi // height in inches
-
-        return sqrt(wi.pow(2) + hi.pow(2))
+    fun getRam() = with(ActivityManager.MemoryInfo()) {
+        activityManager.getMemoryInfo(this)
+        totalMem
     }
 
-    fun getRam(): Long {
-        val am = ctx.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val mi = ActivityManager.MemoryInfo()
-
-        am.getMemoryInfo(mi)
-
-        return mi.totalMem
-    }
-
-    private fun getDisplayMetrics(): DisplayMetrics {
-        val wm = ctx.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val dm = DisplayMetrics()
-
-        wm.defaultDisplay.getRealMetrics(dm)
-
-        return dm
+    private fun getDisplayMetrics() = DisplayMetrics().apply {
+        windowManager.defaultDisplay.getRealMetrics(this)
     }
 }
