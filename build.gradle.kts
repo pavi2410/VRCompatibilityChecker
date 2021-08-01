16994acb08b130484229151fcafd35e3bc7eada0
Pavitra Golchha <pavitragolchha@gmail.com>
2021-08-01 12:03:15 +0530

    Update AGP to 7.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath(kotlin("gradle-plugin", version = "1.5.10"))
     }
 }
