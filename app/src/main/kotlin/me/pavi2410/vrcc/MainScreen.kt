62b5dd4e3e7460f63db70a167ad46b079abf4e28
Pavitra Golchha <hello@pavi2410.me>
2021-12-07 11:30:39 +0530

    Update privacy policy url

--- app/src/main/kotlin/me/pavi2410/vrcc/MainScreen.kt
+++ app/src/main/kotlin/me/pavi2410/vrcc/MainScreen.kt
@@ -227,7 +227,7 @@ fun AboutThisApp() {
 
                     pushStringAnnotation(
                         tag = "privacy-policy",
-                        annotation = "https://pavi2410.github.io/privacy_policy/VR"
+                        annotation = "https://pavi2410.me/privacy_policy/VR"
                     )
 
                     withStyle(style = SpanStyle(color = MaterialTheme.colors.onSecondary)) {
